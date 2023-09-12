ee5f92f5103c39a188105942ec73b5856e28904f
Zoe-life <merlynzaw@gmail.com>
2023-09-11 18:21:37 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main - the main function of the program is hosted here
+ *
+ *Return: 0 when successful
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
